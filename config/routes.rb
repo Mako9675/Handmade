1f481205e8d8716c443c71ab0dcb4b99ffe97674
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-08 05:21:41 +0000

    [add] コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,41 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/unsubscribe'
+    get 'users/withdrawal'
+  end
+  
+  get 'relationships/create'
+  get 'relationships/destroy'
+  get 'searches/search'
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
