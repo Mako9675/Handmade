7603fa96a5ffb97af8173258c69d93be3606a17c
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-14 12:05:13 +0000

    [add] カラムのデフォ削除

--- db/migrate/20230308103532_create_comments.rb
+++ db/migrate/20230308103532_create_comments.rb
@@ -3,8 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
 
       t.timestamps
-      t.integer :user_id,              null: false, default: "", foreign_key: true
-      t.integer :post_id,              null: false, default: "", foreign_key: true
+      t.integer :user_id,              null: false, foreign_key: true
+      t.integer :post_id,              null: false, foreign_key: true
       t.text :comment,            null: false
     end
   end
--- db/migrate/20230308103548_create_favorites.rb
+++ db/migrate/20230308103548_create_favorites.rb
@@ -3,8 +3,8 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
     create_table :favorites do |t|
 
       t.timestamps
-      t.integer :user_id,              null: false, default: "", foreign_key: true
-      t.integer :post_id,              null: false, default: "", foreign_key: true
+      t.integer :user_id,              null: false, foreign_key: true
+      t.integer :post_id,              null: false, foreign_key: true
     end
   end
 end
