59cffeb1a49f8cc87248d1761632ddeb3bb2e400
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-15 06:02:58 +0000

    [add] テーブルのhead整え

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def top
     @genres = Genre.all
-    @post_like_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @post_like_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
   end
 
   def about
