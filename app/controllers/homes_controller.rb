a8f34ad39d96f8bdf167690109f0d8d3f9002c67
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-15 05:46:08 +0000

    [add] ランキング機能実装/FontAwesome追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,11 @@
 class HomesController < ApplicationController
   def top
     @genres = Genre.all
+    @post_like_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
   end
 
   def about
     @genres = Genre.all
   end
+  
 end
