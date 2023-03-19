397dc44589bba7f1a8344228842556e87fa8a856
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-19 08:56:04 +0000

    [add] ロゴ追加/ヘッダーとフッターの色変更

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -24,7 +24,7 @@ class Admin::GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
-    if genre.update(genre_params)
+    if @genre.update(genre_params)
       flash[:notice] = "ジャンルを修正しました"
       redirect_to admin_genres_path
     else
