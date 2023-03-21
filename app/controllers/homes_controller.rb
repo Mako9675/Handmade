27151663159b462bedf53022e72f273a58779aa8
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-21 08:39:31 +0000

    [add] コメントランキング追加/ランキングに制作者と日付追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,7 @@ class HomesController < ApplicationController
   def top
     @genres = Genre.all
     @post_like_ranks = Post.where(id: Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
+    @post_comment_ranks = Post.where(id: Comment.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
   end
 
   def about
