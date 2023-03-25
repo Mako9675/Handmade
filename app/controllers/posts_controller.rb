d7d50d96228e2ac1d95e07441355afb98bf393db
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-25 08:33:50 +0000

    [add] 管理者側から投稿削除したときの移動先修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,7 +59,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.destroy
       flash[:notice] = "投稿を削除しました"
-      redirect_to public_user_path(current_user)
+      redirect_to public_user_path(@post.user)
     else
       flash[:notice] = "削除に失敗しました"
       render 'show'
