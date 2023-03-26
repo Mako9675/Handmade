01a83c0b2aee28b16595b722d2acad650a6d2109
Mako9675 <Mako9675@users.noreply.github.com>
2023-03-26 12:29:25 +0000

    [add] ランキング機能修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
   def top
     @genres = Genre.all
-    @post_like_ranks = Post.includes(:favorites).limit(5).published.sort {|a,b| b.favorites.size <=> a.favorites.size}
-    @post_comment_ranks = Post.includes(:comments).limit(5).published.sort {|a,b| b.comments.size <=> a.comments.size}
+    @post_like_ranks = Post.includes(:favorites).published.limit(5).sort {|a,b| b.favorites.size <=> a.favorites.size}
+    @post_comment_ranks = Post.includes(:comments).published.limit(5).sort {|a,b| b.comments.size <=> a.comments.size}
   end
 
   def about
