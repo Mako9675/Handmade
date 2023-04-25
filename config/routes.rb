8a20b03b6ffcb1979b65461e231582f198742d4d
Mako9675 <Mako9675@users.noreply.github.com>
2023-04-25 11:55:14 +0000

    [add] 管理者トップに管理者作成追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   
   devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
 }
 
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
+  devise_for :admins, skip: [:passwords] ,controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
 }
 
   root to: 'homes#top'
